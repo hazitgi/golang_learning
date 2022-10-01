095d71d431dde6e14800253e336fa1cde42b1eba
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2022-10-01 15:49:20 +0530

    first functions

--- tutorial.go
+++ tutorial.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	// Functions
 
+	// Declaration
+
+	/*
+		 	func function_name(parameter-list)(return_type){
+				body
+			}
+	*/
+
+	fmt.Printf("Area of rectagle is : %d\n", area(12, 50))
+}
+
+func area(length, width int) int {
+	Ar := length * width
+	return Ar
 }
