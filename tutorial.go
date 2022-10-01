fc11ebe53aeded5b7de6f3a74e547de1062e9557
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2022-10-01 15:32:47 +0530

    expression switch state ment

--- tutorial.go
+++ tutorial.go
@@ -3,125 +3,66 @@ package main
 import "fmt"
 
 func main() {
-	// static declaration
-	// var x float64
-	// x = 40356.14
 
-	// Dynamic Declaration
+	// Switch Statement in Go
 
-	// y := 56
+	// Expression Switch
+	// Type Switch
 
-	// Printing
+	// Expression Switch
 
-	// fmt.Println("Hello, World")
-	// fmt.Println(x)
-	// fmt.Printf("x is a type %T\n", x)
-	// fmt.Printf("y is of type %T\n", y)
+	// switch optstatement; optexpression{
+	// 	case expression1: Statement..
+	// 	case expression2: Statement..
+	// 	...
+	// 	default: Statement..
+	// 	}
 
-	// Mixed variable Declaration in GO
+	// // Expression Switch
 
-	// fmt.Println("Mixed Data types declaration")
-	// var a, b, c = 3, 5, "Mohamed Haseeb K"
-	// fmt.Println(a)
-	// fmt.Printf("a data type %T\n", a)
-	// fmt.Println(b)
-	// fmt.Printf("a data type %T\n", b)
-	// fmt.Println(c)
-	// fmt.Printf("a data type %T\n", c)
-
-	// fmt.Println("return multiple values from Function")
-	// fmt.Println(swap("Haseeb", "Mohamed"))
-
-	// var greeting = "greeting"
-
-	// fmt.Printf("normal string: ")
-	// fmt.Printf("%s", greeting)
-
-	// println()
-	// fmt.Printf("hex bytes: ")
-
-	// for i:=0;i<len(greeting);i++{
-	// 	fmt.Printf("%x",greeting[i])
-	// }
-
-	// Arrays
-	// var variable_name [SIZE] variable_type
-	// var balance = [10]float32{2,3,4}
-	// fmt.Println(balance)
-
-	// loops
-
-	// String as a range in the for loop
-	// for i, j := range "XabCd" {
-	// 	fmt.Printf("The index number of %U is %d\n", j, i)
-	// }
-
-	// var value string = "five"
-	// switch value {
-	// case "one":
-	// 	fmt.Println("C#")
-	// case "two", "three":
-	// 	fmt.Println("Go")
-	// case "four", "five", "six":
-	// 	fmt.Println("Java")
+	// switch day := 4; day {
+	// case 1:
+	// 	fmt.Println("Monday")
+	// case 2:
+	// 	fmt.Println("Tuesday")
+	// case 3:
+	// 	fmt.Println("Wednesday")
+	// case 4:
+	// 	fmt.Println("Thursday")
+	// case 5:
+	// 	fmt.Println("Friday")
+	// case 6:
+	// 	fmt.Println("Saturday")
+	// case 7:
+	// 	fmt.Println("Sunday")
 	// default:
-	// 	fmt.Println("He")
+	// 	fmt.Println("Invalid")
 	// }
 
-	// simple for loop
-	// for i := 0; i < 100; i++ {
-	// 	fmt.Println(i)
+	// Exampole 2
+
+	// number1 := 1
+	// number2 := 4
+
+	// switch {
+	// case number1 == 2:
+	// 	fmt.Print("number1 is 1")
+	// case number2 == 4:
+	// 	fmt.Print("number2 is 4")
 	// }
 
-	// infinit loop
-	// for {
-	// 	fmt.Printf("Mohamed Haseeb")
-	// }
+	// Switch statement without default statement
+	// Multiple values in case statement
 
-	// for loop as while Loop
+	value := "five"
 
-	// for condition{
-	// statement..
-	// }
-
-	// value := 0
-	// var sum int
-	// for value <= 50 {
-	// 	fmt.Print(value, "\n")
-	// 	sum += value
-	// 	value++
-	// }
-	// fmt.Print(sum)
-
-	// Simple range in for loop
-
-	// for i, j:= range rvariable{
-	// statement..
-	//  }
-
-	// friends_list := []string{"Mohamed Haseeb", "Akhil", "Faris", "Niyas"}
-	// for i, j := range friends_list {
-	// 	fmt.Println(i, j)
-	// }
-
-	//  Using for loop for strings
-
-	// for i, j := range "Haseeb" {
-	// 	fmt.Printf("The index number of %U is %d \n", j, i)
-	// }
-
-}
-
-func max(num1, num2 int) int {
-	var result int
-	if num1 > num2 {
-		result = num1
-	} else {
-		result = num2
+	switch value {
+	case "one":
+		fmt.Println("Hai case one")
+	case "two", "three":
+		fmt.Println("case is two , three")
+	case "four", "five", "six":
+		fmt.Println("case four, five, six:")
 	}
-	return result
-}
 
-func swap(x, y string) (string, string) {
-	return y, x
 }
