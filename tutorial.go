88475ac1c704754030a73d75bea821f98eeffa65
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2022-10-02 19:38:49 +0530

    nested struct second example

--- tutorial.go
+++ tutorial.go
@@ -7,8 +7,7 @@ func main() {
 		Nested struct
 	*/
 
-
-	/* Example 1 */
+	/* Example 1  Start*/
 
 	result := HR{
 		details: Author{"Mohamed Haseeb K", "Tirurangadi", 25},
@@ -16,6 +15,29 @@ func main() {
 
 	fmt.Println("Details of Author")
 	fmt.Println(result)
+	/* Example 1  End*/
+
+	/* Example 2 Start */
+	result2 := Teacher{
+		name:    "Mohamed Haseeb K",
+		subject: "Go / Golang",
+		exp:     2,
+		details: Student{name: "Aflah", branch: "Commerce", year: 2},
+	}
+
+	fmt.Println("Details of the Teacher")
+	fmt.Println("Teacher's name : ", result2.name)
+	fmt.Println("Subject : ", result2.subject)
+	fmt.Println("Experience : ", result2.exp)
+
+	fmt.Println("Details of student")
+	fmt.Println("Student's name : ", result.details.name)
+	fmt.Println("Student's branch name: ", result2.details.branch)
+	fmt.Println("Year : ", result2.details.year)
+
+	fmt.Println(result2)
+
+	/* Example 2 End */
 
 }
 
@@ -41,3 +63,17 @@ type Author struct {
 type HR struct {
 	details Author
 }
+
+/* Example 2  Start */
+type Student struct {
+	name, branch string
+	year         int
+}
+
+type Teacher struct {
+	name, subject string
+	exp           int
+	details       Student
+}
+
+/* Example 2  End*/
