cb06187b72da0da2046c8fbba38cc6e08010665a
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2022-10-02 19:30:42 +0530

    nested struct example 1

--- tutorial.go
+++ tutorial.go
@@ -3,6 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	/*
+		Nested struct
+	*/
 
+
+	/* Example 1 */
+
+	result := HR{
+		details: Author{"Mohamed Haseeb K", "Tirurangadi", 25},
+	}
+
+	fmt.Println("Details of Author")
+	fmt.Println(result)
+
+}
+
+/* 	Nested struct Syntax */
+/*
+
+	type struct_name_1 struct{
+  		// Fields
+	}
+
+	type struct_name_2 struct{
+  		variable_name  struct_name_1
+	}
+
+*/
+
+/* Example 1 */
+type Author struct {
+	name, branch string
+	year         int
+}
+
+type HR struct {
+	details Author
 }
