6bfc89bff66b4e8b0153007fbf509b54b2fde68a
hazi-tgi <mohamedhaseeb@ateamsoftsolutions.com>
2022-10-14 15:18:52 +0530

    string functions

--- tutorial.go
+++ tutorial.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println("Strings in Golang")
+
+	str1 := "!! Mohamed Haseeb K !!"
+
+	fmt.Println("String before trimming")
+	fmt.Println("String 1 : ", str1)
+
+	trim_str1 := strings.Trim(str1, "!")
+
+	fmt.Println("Strings after trimming : ")
+	fmt.Println("Result 1 : ", trim_str1)
 
 }
